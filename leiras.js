51e55ef9cc24d59ec540be838a31636e6634136d
Norbipolak <142392455+Norbipolak@users.noreply.github.com>
2023-12-20 21:04:56 +0100

    update

---
+++ leiras.js
@@ -0,0 +1,25 @@
+/*
+kapcsolat.html-en belül a main-be bemásoltuk ezt
+*/
+
+/*<div class="box">
+<h3>Kapcsolati adatok</h3>
+<ul>
+    <li>cégjegyzékszám: 01-09-1231645</li>
+    <li>adószám: 12345678-2-45</li>
+    <li>székhely:1111 Bp. Cég utca 6</li>
+</ul>
+</div>
+*/
+/*
+De ezt úgy is meg lehet csinálni, hogy ->
+*/
+/*
+<div class="container">
+<div class="box">
+    <h2>Kapcsolati adatok</h2>
+    <h3>cégjegyzékszám: 01-09-1231645</h3>
+    <h3>adószám: 12345678-2-45</h3>
+    <h3>székhely:1111 Bp. Cég utca 6</h3>
+</div>
+*/
